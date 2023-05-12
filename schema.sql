90e14499517b3b05dda3860d12cd6b7a46c852b5
Maxim <126081409+Maxim-Levchenko@users.noreply.github.com>
2023-05-13 01:01:00 +0300

    +reviews

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS purchased_movies;
 DROP TABLE IF EXISTS balances;
+DROP TABLE IF EXISTS reviews;
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,7 +14,7 @@ CREATE TABLE users (
 CREATE TABLE purchased_movies (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
-	movie_name TEXT NOT NULL,
+    movie_name TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
@@ -22,3 +23,19 @@ CREATE TABLE balances (
     balance REAL NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
+
+CREATE TABLE reviews (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    movie_id INTEGER NOT NULL,
+    rating INTEGER NOT NULL,
+    first_name1 TEXT NOT NULL,
+    last_name1 TEXT NOT NULL,
+    comment TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (first_name1) REFERENCES users (first_name),
+    FOREIGN KEY (last_name1) REFERENCES users (last_name)
+);
+
+SELECT AVG(rating) as avg_rating FROM reviews WHERE movie_id = ?
